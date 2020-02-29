35ac9f5580aa611fa520a12538e94807640a6361
Alex Sarafian <dev1978@outlook.com>
2020-02-29 20:57:37 +0100

    AppVeyor

--- CI/Test-Module.ps1
+++ CI/Test-Module.ps1
@@ -1,7 +1,12 @@
 param(
+    [Parameter(Mandatory=$true,ParameterSetName="AppVeyor")]
+    [switch]$AppVeyor,
+    [Parameter(Mandatory=$false,ParameterSetName="Console")]
     [switch]$EnableExit=$false,
+    [Parameter(Mandatory=$false,ParameterSetName="Console")]
     [switch]$PassThru=$false
 )
+
 $pesterScript=@(
     @{
         Path="$PSScriptRoot\..\Src\Modules"
@@ -10,4 +15,16 @@ $pesterScript=@(
         Path="$PSScriptRoot\..\Src\Tests\Modules"
     }
 )
-Invoke-Pester -Script $pesterScript @PSBoundParameters
+
+switch($PSCmdlet.ParameterSetName) {
+    'AppVeyor' {
+        $res = Invoke-Pester -Script $pesterScript -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru
+        (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
+        if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed."}
+    }
+    'Console' {
+        Invoke-Pester -Script $pesterScript @PSBoundParameters
+    }
+}
+
+
