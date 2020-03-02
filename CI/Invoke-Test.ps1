9eda5d587806d565abea07ed5c110b815f04f546
Alex Sarafian <dev1978@outlook.com>
2020-03-02 21:42:56 +0100

    AV

--- CI/Invoke-Test.ps1
+++ CI/Invoke-Test.ps1
@@ -18,7 +18,7 @@ $splat=@{
     PassThru=$true
     OutputFormat="NUnitXml"
     OutputFile=$outputFile
-#    PesterOption=New-PesterOption -TestSuiteName "$($_ -join ",") tags tests summary"
+    PesterOption=New-PesterOption -TestSuiteName "$($_ -join ",") tags tests summary"
     ExcludeTag=$ExcludeTag
     Tag=$Tag
 }
@@ -49,7 +49,6 @@ if($CodeCoverage)
     }
 }
 
-$PSCmdlet.ParameterSetName
 switch($PSCmdlet.ParameterSetName) {
     'AppVeyor' {
         (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $outputFile)
@@ -61,85 +60,3 @@ switch($PSCmdlet.ParameterSetName) {
 if ($pesterResult.FailedCount -gt 0) { 
      throw "$($pesterResult.FailedCount) tests failed."
 }
-
-
-
-<#
-$pesterScript=@(
-    @{
-        Path="$PSScriptRoot\..\Src"
-    }
-)
-switch($PSCmdlet.ParameterSetName) {
-    'AppVeyor' {
-        $res1 = Invoke-Pester -Script $pesterScript -OutputFormat NUnitXml -OutputFile TestsResults1.xml -PassThru -ExcludeTag "GH-1461" -Tag "M1"
-        (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults1.xml))
-        $res2 = Invoke-Pester -Script $pesterScript -OutputFormat NUnitXml -OutputFile TestsResults2.xml -PassThru -ExcludeTag "GH-1461" -Tag "M2"
-        (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults2.xml))
-        if (($res1.FailedCount -gt 0) -or ($res2.FailedCount -gt 0)) { throw "$($res1.FailedCount+$res2.FailedCount) tests failed."}
-    }
-    'Console' {
-        Invoke-Pester -Script $pesterScript @PSBoundParameters -ExcludeTag "GH-1461" -
-    }
-}
-#>
-<#
-$pesterSplats=@(
-    @("M1","Cmdlet"),
-    @("M1","Module"),
-    @("M1","Script"),
-    @("M2","Cmdlet"),
-    @("M2","Module"),
-    @("M2","Script")
-)|ForEach-Object {
-    $outputFile=[System.IO.Path]::GetTempFileName()+".xml"
-    $codeCoveragePath=$outputFile.Replace(".xml",".codecoverage.xml")
-    @{
-        Script=$srcPath
-        CodeCoverage=Get-ChildItem -Path $srcPath -Exclude @("*.Tests.ps1","*.NotReady.ps1") -Filter "*.ps1" -Recurse|Select-Object -ExpandProperty FullName
-        CodeCoverageOutputFile=$codeCoveragePath
-        PassThru=$true
-        OutputFormat="NUnitXml"
-        OutputFile=$outputFile
-        PesterOption=New-PesterOption -TestSuiteName "$($_ -join ",") tags tests summary"
-        ExcludeTag="GH-1461"
-        Tag=$_
-    }
-}   
-
-$pesterResults=$pesterSplats|ForEach-Object {
-    $splat=$_
-    $pesterResult=Invoke-Pester @splat
-    $pesterResult | Add-Member -Name "TestResultPath" -Value $splat.OutputFile -MemberType NoteProperty
-    $pesterResult | Add-Member -Name "CodeCoveragePath" -Value $splat.CodeCoverageOutputFile -MemberType NoteProperty
-    $pesterResult
-}
-
-switch($PSCmdlet.ParameterSetName) {
-    'AppVeyor' {
-        $pesterResults|ForEach-Object {
-            (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path -Path $_.TestResultPath))
-        }
-    }
-    'Console' {
-    }
-}
-
-$pesterResults | Select-Object TotalCount,PassedCount,FailedCount,SkippedCount,PendingCount,Time,@{
-    Name="CommandsAnalyzed"
-    Expression={$_.CodeCoverage.NumberOfCommandsAnalyzed}
-},@{
-    Name="FilesAnalyzed"
-    Expression={$_.CodeCoverage.NumberOfFilesAnalyzed}
-},@{
-    Name="CommandsExecuted"
-    Expression={$_.CodeCoverage.NumberOfCommandsExecuted}
-},@{
-    Name="CommandsMissed"
-    Expression={$_.CodeCoverage.NumberOfCommandsMissed}
-}|Format-Table
-$totalFailedCount=$pesterResults | Measure-Object -Sum -Property FailedCount|Select-Object -ExpandProperty Sum
-if ($totalFailedCount -gt 0) { 
-    throw "$totalFailedCount tests failed."
-}
-#>
