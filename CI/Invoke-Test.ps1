7121af084a79614bb800387991777d1fb3aef6c4
Alex Sarafian <dev1978@outlook.com>
2020-03-02 21:33:22 +0100

    AppVeyor

--- CI/Invoke-Test.ps1
+++ CI/Invoke-Test.ps1
@@ -1,12 +1,65 @@
 param(
     [Parameter(Mandatory=$true,ParameterSetName="AppVeyor")]
     [switch]$AppVeyor,
+    [Parameter(Mandatory=$false,ParameterSetName="AppVeyor")]
     [Parameter(Mandatory=$false,ParameterSetName="Console")]
-    [switch]$EnableExit=$false,
+    [string[]]$Tag=$null,
+    [Parameter(Mandatory=$false,ParameterSetName="AppVeyor")]
     [Parameter(Mandatory=$false,ParameterSetName="Console")]
-    [switch]$PassThru=$false
+    [switch]$CodeCoverage=$false
 )
 $srcPath=Resolve-Path -Path "$PSScriptRoot\..\Src"
+$outputFile=[System.IO.Path]::GetTempFileName()+".xml"
+$splat=@{
+    Script=$srcPath
+    PassThru=$true
+    OutputFormat="NUnitXml"
+    OutputFile=$outputFile
+    PesterOption=New-PesterOption -TestSuiteName "$($_ -join ",") tags tests summary"
+#    ExcludeTag="GH-1461"
+    Tag=$Tag
+}
+if($CodeCoverage)
+{
+    $codeCoveragePath=$outputFile.Replace(".xml",".codecoverage.xml")
+    $splat+=@{
+        CodeCoverage=Get-ChildItem -Path $srcPath -Exclude @("*.Tests.ps1","*.NotReady.ps1","Src\Tests\**") -Filter "*.ps1" -Recurse|Select-Object -ExpandProperty FullName
+        CodeCoverageOutputFile=$codeCoveragePath        
+    }
+}
+
+$pesterResult=Invoke-Pester @splat
+if($CodeCoverage)
+{
+    $pesterResult|Select-Object @{
+        Name="CommandsAnalyzed"
+        Expression={$_.CodeCoverage.NumberOfCommandsAnalyzed}
+    },@{
+        Name="FilesAnalyzed"
+        Expression={$_.CodeCoverage.NumberOfFilesAnalyzed}
+    },@{
+        Name="CommandsExecuted"
+        Expression={$_.CodeCoverage.NumberOfCommandsExecuted}
+    },@{
+        Name="CommandsMissed"
+        Expression={$_.CodeCoverage.NumberOfCommandsMissed}
+    }
+}
+
+switch($PSCmdlet.ParameterSetName) {
+    'AppVeyor' {
+        (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path -Path $outputFile))
+    }
+    'Console' {
+
+    }
+}
+if ($pesterResult.FailedCount -gt 0) { 
+     throw "$($pesterResult.FailedCount) tests failed."
+}
+
+
+
 <#
 $pesterScript=@(
     @{
@@ -26,7 +79,7 @@ switch($PSCmdlet.ParameterSetName) {
     }
 }
 #>
-
+<#
 $pesterSplats=@(
     @("M1","Cmdlet"),
     @("M1","Module"),
@@ -85,3 +138,4 @@ $totalFailedCount=$pesterResults | Measure-Object -Sum -Property FailedCount|Sel
 if ($totalFailedCount -gt 0) { 
     throw "$totalFailedCount tests failed."
 }
+#>
